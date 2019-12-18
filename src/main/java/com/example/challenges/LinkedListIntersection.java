822211bebd2612299f2beb5d1a87f3fd01707841
Glen Worsley <glen.worsley@shinesolutions.com>
2019-12-18 12:04:13 +1100

    functional working version

--- src/main/java/com/example/challenges/LinkedListIntersection.java
+++ src/main/java/com/example/challenges/LinkedListIntersection.java
@@ -5,27 +5,14 @@ import java.util.*;
 public class LinkedListIntersection {
 
     /**
-     * Returns value at intersecting node of the 2 linked lists.
+     * Returns Optional for Integer at intersecting node of the 2 linked lists.
      * Needs to be done in O(M+N) time where M = length of listA and N = length of listB.
      * @param listA
      * @param listB
-     * @return value at intersecting node of the 2 linked lists.  If no intersection, null is returned.
+     * @return value at intersecting node of the 2 linked lists.  If no intersection, Optional will be empty.
      */
-    public Integer getValueAtIntersectingNode(LinkedList<Integer> listA, LinkedList<Integer> listB) {
-
-        Integer valueAtIntersectingNode = null;
-
-         //load all ints in listA into a Set
+    public Optional<Integer> getValueAtIntersectingNode(LinkedList<Integer> listA, LinkedList<Integer> listB) {
         Set<Integer> listAValues = new HashSet<>(listA);
-
-        //loop through listB until find a value in the Set
-        ListIterator<Integer> iterator = listB.listIterator();
-        while (iterator.hasNext() && valueAtIntersectingNode == null) {
-            Integer next = iterator.next();
-            if (listAValues.contains(next))
-                valueAtIntersectingNode = next;
-        }
-
-        return valueAtIntersectingNode;
+        return listB.stream().filter(element -> listAValues.contains(element)).findFirst();
     }
 }
--- src/test/java/com/example/challenges/LinkedListIntersectionTest.java
+++ src/test/java/com/example/challenges/LinkedListIntersectionTest.java
@@ -2,28 +2,30 @@ package com.example.challenges;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class LinkedListIntersectionTest {
 
+    private LinkedListIntersection linkedListIntersection = new LinkedListIntersection();
+
     @Test
     void getValueAtIntersectingNode() {
+        LinkedList<Integer> listA = new LinkedList<>(Arrays.asList(3,7,8,10));
+        LinkedList<Integer> listB = new LinkedList<>(Arrays.asList(99,1,8,10));
+        Optional<Integer> result = linkedListIntersection.getValueAtIntersectingNode(listA, listB);
+        assertTrue(result.isPresent());
+        assertEquals(8, result.get());
+    }
 
-        LinkedList<Integer> listA = new LinkedList<>();
-        listA.add(3);
-        listA.add(7);
-        listA.add(8);
-        listA.add(10);
-
-        LinkedList<Integer> listB = new LinkedList<>();
-        listB.add(99);
-        listB.add(1);
-        listB.add(8);
-        listB.add(10);
-
-        LinkedListIntersection linkedListIntersection = new LinkedListIntersection();
-        assertEquals(8, linkedListIntersection.getValueAtIntersectingNode(listA, listB));
+    @Test
+    void returnEmptyIfNoIntersectingNode() {
+        LinkedList<Integer> listA = new LinkedList<>(Arrays.asList(3,7,8,10));
+        LinkedList<Integer> listB = new LinkedList<>(Arrays.asList(99,1,9,11));
+        Optional<Integer> result = linkedListIntersection.getValueAtIntersectingNode(listA, listB);
+        assertFalse(result.isPresent());
     }
 }
