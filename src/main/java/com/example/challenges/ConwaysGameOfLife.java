df393fc82fde914d08e441801e0b8f2021cbf902
Glen Worsley <glen.worsley@shinesolutions.com>
2020-01-07 17:29:16 +1100

    start adding code to apply rule1. feature is incomplete.

--- src/main/java/com/example/challenges/ConwaysGameOfLife.java
+++ src/main/java/com/example/challenges/ConwaysGameOfLife.java
@@ -23,9 +23,32 @@ public class ConwaysGameOfLife {
                         false, true, true, false
                 },
         };
-        //GameBoard resultBoard = new GameBoard(result);
-
+        GameBoard resultBoard = applyRule1(gameBoard);
         return resultBoard;
+    }
+
+    /*
+     * traverse the given board.
+     * For each cell, if cell is live & cell has less than 2 live neighbours, cell dies.
+     * @param gameBoard
+     * @return
+     */
+    private GameBoard applyRule1(GameBoard gameBoard) {
+
+        boolean[][] data = new boolean[gameBoard.isAlive.length][gameBoard.isAlive.length];  //TODO: assumes board is square!
+        for (int i = 0; i < gameBoard.isAlive.length; i++) {
+            for (int j = 0; j < gameBoard.isAlive[i].length; j++) {
+                data[i][j] = gameBoard.isAlive[i][j] && hasAtLeastTwoLiveNeighbors(gameBoard, i, j);
+            }
+        }
+        return new GameBoard(data);
+    }
+
+    private boolean hasAtLeastTwoLiveNeighbors(GameBoard gameBoard, int i, int j) {
+        int liveNeighbors = 0;
+        boolean neighbourAbove = gameBoard.isAlive[i][j+1];
+        return false;
+
     }
 
 
